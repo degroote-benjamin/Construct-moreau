23209cfff902a8d1e6172b5737e167e2d9967eaf
benjamin <degroote.benjamin@gmail.com>
2017-09-27 16:23:20 +0200

    add list in the foreach for list all task for each step

--- view/detail.php
+++ view/detail.php
@@ -1,15 +1,22 @@
 <h2><?php echo $valuedataprojectid['project_name'];?></h2>
 
-<?php foreach ($datastepid as $key => $valuedatastepid) {?>
+<ul>
+<?php foreach ($datastepid as $key => $valuedatastepid) {
+    ?>
 
-<p><?php echo $valuedatastepid['step_name']?></p>
+<li><?php echo $valuedatastepid['step_name']?>
+  <ul>
 <?php foreach ($datatask as $key => $value) {
-if ($value['id_step'] == $valuedatastepid['id_step']){
-?>
-<h1><?php echo $value['id_step'] ?></h1>
+        if ($value['id_step'] == $valuedatastepid['id_step']) {
+            ?>
+<li><?php echo $value['task_name'] ?></li>
 
 <?php
-}
-
-}
+        }
+    }
+    ?>
+  </ul>
+  </li>
+    <?php
 } ?>
+</ul>
