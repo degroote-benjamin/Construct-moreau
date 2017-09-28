7b7081e291617100b2ccff74df665e95e3b4273f
benjamin <degroote.benjamin@gmail.com>
2017-09-28 15:09:40 +0200

    add some css for table and form

--- view/space.php
+++ view/space.php
@@ -1,5 +1,6 @@
-<table class="table">
-  <tr>
+<div class="container">
+<table class="table table-responsive d-flex justify-content-center">
+  <tr class="thead-inverse">
     <th>project number</th>
     <th>project name</th>
     <th>project description</th>
@@ -13,7 +14,7 @@
         ?>
       <tr>
         <td><?php echo $valueproject['id_project']; ?></td>
-        <td><?php echo $valueproject['project_name'] ?></td>
+        <td><a href="../control/step_area.php?id_project=<?php echo $valueproject['id_project']?>"><?php echo $valueproject['project_name'] ?></a></td>
         <td><?php echo $valueproject['description_project'] ?></td>
         <td><?php echo $valueproject['category_name'] ?></td>
         <td><?php echo $valueproject['end_date'] ?></td>
@@ -28,9 +29,19 @@
    ?>
 </table>
 
-<form class="" action="project_area.php" method="post">
+</div>
+
+<div class="container">
+<form class="text-center" action="project_area.php" method="post">
+  <div class="form-group">
   <input type="text" name="project_name" value="" placeholder="project_name">
+
+</div>
+<div class="form-group">
   <input type="text" name="description_project" value="" placeholder="description">
+
+</div>
+<div class="form-group">
     <select class="" name="id_category">
       <?php
       foreach ($datacategory as $valuecategory) {
@@ -38,6 +49,13 @@
       <option value="<?php echo $valuecategory['id_category'] ?>"><?php echo $valuecategory['category_name'] ?></option>
       <?php } ?>
     </select>
+
+  </div>
+  <div class="form-group">
     <input type="date" name="end_date" value="" placeholder="end_date">
-    <input type="submit" name="submitproject" value="submit">
+
+  </div>
+    <input type="submit" name="submitproject" value="submit" class="btn btn-primary">
 </form>
+
+</div>
