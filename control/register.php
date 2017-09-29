3cbd9707d65c350d6149df326e7c69946b9e50e7
benjamin <degroote.benjamin@gmail.com>
2017-09-29 13:58:05 +0200

    add form on register.php and create control

---
+++ control/register.php
@@ -0,0 +1,12 @@
+<?php
+include_once('../model/data.php');
+
+include 'header.php';
+
+// if id project doesn't exist vue is index
+
+
+    include '../view/register.php';
+
+
+include 'footer.php';
---
+++ view/register.php
@@ -0,0 +1,17 @@
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please register</h2>
+        <label for="inputPseudo" class="sr-only">Pseudo</label>
+        <input type="text" name="pseudo" id="inputPseudo" class="form-control" placeholder="Pseudo" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="pass" id="inputPassword" class="form-control" placeholder="Password" required>
+        <label for="inputPassword1" class="sr-only">Confirm Password</label>
+        <input type="password" name="passconf" id="inputPassword1" class="form-control" placeholder="Confirm Password" required>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name ="email" id="inputEmail" class="form-control" placeholder="Email address" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div>
