4cb90f739974bdea76de0fbbfd7cc26e5489909a
benjamin <degroote.benjamin@gmail.com>
2017-10-02 09:23:39 +0200

    add session when login is true

--- control/login.php
+++ control/login.php
@@ -1,15 +1,21 @@
 <?php
 include_once('../model/data.php');
 session_start();
-
 $user = get_user($_POST['pseudo']);
 
+// if click on login submit
 if (isset($_POST['submitlogin'])) {
-  if(isset($_POST['pseudo']) and isset($_POST['pass'])){
-    if (password_verify($_POST['pass'], $user['pass'])) {
-      
+    // if isset field pseudo and pass
+    if (isset($_POST['pseudo']) and isset($_POST['pass'])) {
+        // if pass verify is true
+        if (password_verify($_POST['pass'], $user['pass'])) {
+            include 'index.php';
+            $_SESSION['pseudo']=$user['pseudo'];
+            $_SESSION['id']=$user['id_user'];
+        } else {
+            include 'header.php';
+            include '../view/login.php';
+            include 'footer.php';
+        }
     }
-  }
 }
-
-include '../view/login.php';
