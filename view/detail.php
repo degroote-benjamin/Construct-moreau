e038d550b546a2c66a85f41311cf409dff923304
benjamin <degroote.benjamin@gmail.com>
2017-10-03 09:13:13 +0200

    add comment for double foreach + if

--- view/detail.php
+++ view/detail.php
@@ -3,13 +3,16 @@
 <h4>project description :</h4>
 <p><?php echo $valuedataprojectid['description_project'];?></p>
 <ul class="list-group">
+  <!-- data step per id  -->
     <?php foreach ($datastepid as $key => $valuedatastepid) {
     ?>
 
     <li> step :
         <?php echo $valuedatastepid['step_name']?>
         <ul>
+          <!-- select * form task  -->
             <?php foreach ($datatask as $key => $value) {
+              // select id step in select all task and in step per id and filter the task for each step
         if ($value['id_step'] == $valuedatastepid['id_step']) {
             ?>
             <li> task :
